468790a65d5dafcf870162e950db2cb8ab0f43a1
Jay Luker <lbjay@reallywow.com>
2012-12-08 13:57:22 -0500

    Forgot to bump version number to trigger install updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Solrpy',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/flask-solrpy',
     license='MIT',
     author='Jay Luker',
